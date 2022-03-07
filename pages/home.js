b591a5764234ab465890534fa4c8e240bd1660b8
Erin <erinllouise3@gmail.com>
2022-03-08 10:54:34 +1300

    pre scroll attempt

--- pages/home.js
+++ pages/home.js
@@ -7,14 +7,22 @@ function Home() {
       <div className="darkSection">
         <section>
           <h1>ERIN LOUISE</h1>
-          <navDark />
+          <div className="container container_solid">
+            <div className="title_wrapper">
+              <navDark />
+            </div>
+          </div>
         </section>
         <section>
           <h1>ERIN LOUISE</h1>
           {/* <h1>PRODUCT DESIGNER/</h1>
           <h1>FRONT END DEVELOPER/</h1>
           <h1>CREATIVE TECHNOLOGIST</h1> */}
-          <navDark />
+          <div className="container container_image" aria-hidden="true">
+            <div className="title_wrapper">
+              <navDark />
+            </div>
+          </div>
         </section>
       </div>
     </>
